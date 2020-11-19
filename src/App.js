0226bd3c738bfcb7ff3c2f790ae838db0c172303
Larisse <larisse.lima2@gmail.com>
2020-11-19 20:11:27 -0300

    Adiciona API externa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Github from './Components/Github'
 import Titulo from './Components/Titulo'
 
 function App() {
   return (
     <div>
       <Titulo text="Consumindo API em React" />
+      <Github />
     </div>
   );
 }
